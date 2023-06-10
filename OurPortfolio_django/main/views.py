43d340a798a9544a0b087b2a905b37f3f225ff56
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2023-06-10 13:42:01 +0530

    All working with 4 templates and some errors in temp3 to fix

--- OurPortfolio_django/main/views.py
+++ OurPortfolio_django/main/views.py
@@ -5,16 +5,30 @@ from .models import Portfolio
 # Create your views here.
 def index(request):
     # user = Portfolio.objects.get(user_name=user_name)
-    return render(request, "portfolio1/home.html")
+    return render(request, "portfolio1/home1.html")
 
-def portfolios(request, id):
-    user = Portfolio.objects.get(id=id)
-    return render(request, "portfolio1/portfolios1.html", {
-        "user": user
-    })
+# def portfolios(request, id):
+#     user = Portfolio.objects.get(id=id)
+#     return render(request, "portfolio1/portfolios1.html", {
+#         "user": user
+#     })
 
 def portfolios_username(request, username):
     user = Portfolio.objects.get(user_name=username)
-    return render(request, "portfolio1/portfolios1.html", {
+    if user.porfolioTemplate == "BLUE":
+        return render(request, "portfolio1/portfolio3.html", {
         "user": user
-    })
+        })
+    elif user.porfolioTemplate == "CREAM":
+        return render(request, "portfolio1/portfolio2.html", {
+        "user": user
+        })
+    elif user.porfolioTemplate == "DARK":
+        return render(request, "portfolio1/portfolio4.html", {
+        "user": user
+        })
+    else:
+        return render(request, "portfolio1/portfolio1.html", {
+        "user": user
+        })
+    
---
+++ OurPortfolio_django/readme.md
@@ -0,0 +1,19 @@
+# What to do 
+
+- [ ] Learn about exception handling
+- Add home page.
+- Decide what are the imp
+- 
+HTML files
+
+- Remove unnecessary tags
+- Template inheritence [ Not required untill you come to 'all complete' stage]
+- Adding basic annimation
+- 
+
+
+## Good but not nessary now
+- authorization
+- authentication
+- Execption handling
+- 
