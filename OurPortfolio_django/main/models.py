bfb6e2a034e290cf86737fc63863121d7c9c24c6
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2023-06-22 17:51:43 +0530

    Before adding models and other data

--- OurPortfolio_django/main/models.py
+++ OurPortfolio_django/main/models.py
@@ -17,16 +17,39 @@ class Portfolio(models.Model):
   user_name = models.CharField(max_length=100, unique=True, null=True)
   # Use first name and last
   name = models.CharField(max_length=100)
+
+  # ? Ask for alternative or better name for jobtitle
   jobTitle = models.CharField(max_length=50)
   shortDescription = models.CharField(max_length=300)
+  
+# Keeps im
   profilePic = models.ImageField(upload_to="images", null=True)
   porfolioTemplate = models.CharField(max_length=10, choices=THEMES, default="DEFAULT")
+
+  # SOCIALS
+
+  # twitter = 
   # linkedIn = models.URLField(null=True)
   # github = models.URLField(null=True)
   # mail = models.EmailField(null=True)
+  # insta
+  
   # aboutMe = models.CharField(max_length=5000, null=True)
   # skills = models.CharField(max_length=300, null=True)
+    # what level of expertise 
+  # contact me
+  # shareMy profile
 
   # Methods
   def __str__(self):
     return self.user_name
+  
+
+# Good but not neccesary
+# 1. Hire me
+# 2. Showcase their work / projects
+# *3. Certification
+# 4. Services offered
+# 5. Testimonials
+# 6. Download as pdf
+# *7. RESUME/CV LINK
--- OurPortfolio_django/readme.md
+++ OurPortfolio_django/readme.md
@@ -1,10 +1,10 @@
 # What to do 
 
-- [ ] Learn about exception handling
+- [c ] Learn about exception handling
 - Add home page.
 - Decide what are the imp
 - 
-HTML files
+HTML fileshttps://www.wix.com/portfolio-website?utm_source=google&utm_medium=cpc&utm_campaign=16242175905^134377094078&experiment_id=^^582523585419^^_DSA&gclid=CjwKCAjwvpCkBhB4EiwAujULMh7r9qNsTtAEFOBQ619BqyCourzC6FQdTUQpUpSQS9hVE6Sxw1OOBBoCT68QAvD_BwE
 
 - Remove unnecessary tags
 - Template inheritence [ Not required untill you come to 'all complete' stage]
@@ -17,3 +17,4 @@ HTML files
 - authentication
 - Execption handling
 - 
+
